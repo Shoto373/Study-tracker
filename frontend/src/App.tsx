0a5773eda0d7615be90089570ce9586fefcad67a
Shoto <maksimlarin2007@mail.ru>
2026-06-24 16:19:08 +0300

    поставлен реакт, ts, front с vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Study Tracker</h1>
+      <p>Скоро здесь будет трекер учебной активности</p>
+    </div>
+  )
+}
+
+export default App
